47bfe911f593960b4ec2ae933913f2124255112f
Hunter Johnson <hhjohnso@indiana.edu>
2016-07-25 13:14:09 -0400

    Create random_forest_example.py

---
+++ random-forest/random_forest_example.py
@@ -0,0 +1,45 @@
+
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+
+from sklearn.datasets import load_iris
+
+import pandas as pd
+import numpy as np
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+df['species'] = pd.Factor(iris.target, iris.target_names)
+df.head()
+
+train, test = df[df['is_train']==True], df[df['is_train']==False]
+
+features = df.columns[:4]
+clf = RandomForestClassifier(n_jobs=2)
+y, _ = pd.factorize(train['species'])
+clf.fit(train[features], y)
+
+preds = iris.target_names[clf.predict(test[features])]
+pd.crosstab(test['species'], preds, rownames=['actual'], colnames=['preds'])
+
+
+
+def main():
+    #create the training & test sets, skipping the header row with [1:]
+    dataset = genfromtxt(open('Data/train.csv','r'), delimiter=',', dtype='f8')[1:]    
+    target = [x[0] for x in dataset]
+    train = [x[1:] for x in dataset]
+    test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
+    
+    #create and train the random forest
+    #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+    rf = RandomForestClassifier(n_estimators=100)
+    rf.fit(train, target)
+
+    savetxt('Data/submission2.csv', rf.predict(test), delimiter=',', fmt='%f')
+
+if __name__=="__main__":
+    main()
+    
+  
